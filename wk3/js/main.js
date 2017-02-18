6f9e7e8a63d5ccdd22860d6a5aeddc89f26fc0c0
Chloe Atchue-Mamlet <chloe793@gmail.com>
2017-02-18 12:47:35 -0500

    omg finally responsive video

--- wk3/js/main.js
+++ wk3/js/main.js
@@ -11,3 +11,18 @@ function toggleBurger() {
   burger.classList.toggle('is-open');
   menu.classList.toggle('is-visible');
 }
+
+// RESPONSIVE VIDEO
+// With help from: https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+
+var video = document.getElementById('video');
+// Numbers come from YouTube's assigned width and height for this video
+var aspectRatio = 560 / 315;
+var videoContainer = document.getElementById('trailer');
+var videoContainerWidth = videoContainer.clientWidth;
+var videoContainerHeight = videoContainer.clientHeight;
+var newVideoWidth = videoContainerWidth;
+var newVideoHeight = videoContainerWidth / aspectRatio;
+
+video.setAttribute('width', newVideoWidth);
+video.setAttribute('height', newVideoHeight);
