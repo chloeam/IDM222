023ff03413441b689bdfbe7d826ab95404a45d33
Chloe Atchue-Mamlet <chloe793@gmail.com>
2018-03-23 22:38:02 -0400

    finished final version hopefully

---
+++ final/js/main.js
@@ -0,0 +1,30 @@
+// RESPONSIVE NAVIGATION
+
+burger.addEventListener("click", toggleBurger);
+
+function toggleBurger() {
+  console.log('toggleBurger called');
+
+  var burger = document.getElementById('burger');
+  var menu = document.getElementById('menu');
+  var infoBlock = document.getElementById('info-block');
+
+  burger.classList.toggle('is-open');
+  menu.classList.toggle('is-visible');
+  infoBlock.style.opacity = infoBlock.style.opacity === '0.2' ? '' : '0.2';
+}
+
+// RESPONSIVE VIDEO
+// With help from: https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+
+var video = document.getElementById('video');
+// Numbers come from 16:9 aspect ratio
+var aspectRatio = 560 / 315;
+var videoContainer = document.getElementById('trailer');
+var videoContainerWidth = videoContainer.clientWidth;
+var videoContainerHeight = videoContainer.clientHeight;
+var newVideoWidth = videoContainerWidth;
+var newVideoHeight = videoContainerWidth / aspectRatio;
+
+video.setAttribute('width', newVideoWidth);
+video.setAttribute('height', newVideoHeight);
